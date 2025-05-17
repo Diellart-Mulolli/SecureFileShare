0c93f4f56485dd6d98684ee638ca940dd171a22e
DiellartMulolli <diellart.mulolli@student.uni-pr.edu>
2025-05-17 19:52:48 +0200

    File handling

--- script/fileManager.js
+++ script/fileManager.js
@@ -21,3 +21,18 @@ export async function encryptFile(file, aesKey) {
     const encrypted = await window.crypto.subtle.encrypt({ name: "AES-GCM", iv }, aesKey, fileData);
     return { iv: Array.from(iv), encryptedData: new Uint8Array(encrypted) };
 }
+
+export function readFileAsArrayBuffer(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = reject;
+        reader.readAsArrayBuffer(file);
+    });
+}
+
+export function formatFileSize(bytes) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + ' ' + sizes[i];
+}
