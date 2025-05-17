536c6b5aec16970143721db0c4008194894b4e3d
diellfazliu <diell.fazliu@student.uni-pr.edu>
2025-05-17 18:47:34 +0200

    Adding generate KeyPair function

--- auth.js
+++ auth.js
@@ -1,1 +1,18 @@
 export let currentUser = null;
+
+export async function generateKeyPair() {
+    const keyPair = await window.crypto.subtle.generateKey(
+        {
+            name: "RSA-OAEP",
+            modulusLength: 2048,
+            publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+            hash: "SHA-256"
+        },
+        true,
+        ["encrypt", "decrypt"]
+    );
+    return {
+        publicKey: await window.crypto.subtle.exportKey("jwk", keyPair.publicKey),
+        privateKey: await window.crypto.subtle.exportKey("jwk", keyPair.privateKey)
+    };
+}
