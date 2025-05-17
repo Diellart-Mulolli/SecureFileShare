e08a9325a7a92a89e0df1b4ddb84d21739cc38a6
DiellartMulolli <diellart.mulolli@student.uni-pr.edu>
2025-05-17 19:52:06 +0200

    AES encryption

--- script/fileManager.js
+++ script/fileManager.js
@@ -7,3 +7,17 @@ export async function generateAesKey() {
         ["encrypt", "decrypt"]
     );
 }
+
+export async function encryptText(text, aesKey) {
+    const iv = window.crypto.getRandomValues(new Uint8Array(12));
+    const encoded = new TextEncoder().encode(text);
+    const encrypted = await window.crypto.subtle.encrypt({ name: "AES-GCM", iv }, aesKey, encoded);
+    return { iv: Array.from(iv), encryptedData: new Uint8Array(encrypted) };
+}
+
+export async function encryptFile(file, aesKey) {
+    const iv = window.crypto.getRandomValues(new Uint8Array(12));
+    const fileData = await readFileAsArrayBuffer(file);
+    const encrypted = await window.crypto.subtle.encrypt({ name: "AES-GCM", iv }, aesKey, fileData);
+    return { iv: Array.from(iv), encryptedData: new Uint8Array(encrypted) };
+}
