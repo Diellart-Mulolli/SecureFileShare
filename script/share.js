2c55dc9bdbed285c2561fc0e1dd325676240405c
Delvina Elshani <delvina.elshani@student.uni-pr.edu>
2025-05-17 20:10:01 +0200

    Shto kontrollin për ekzistencën e përdoruesit dhe skedarit

---
+++ script/share.js
@@ -0,0 +1,5 @@
+const recipient = tempDB.users.find(u => u.username === recipientUsername);
+if (!recipient) return Callback("Perdoruesi nuk u gjet");
+
+const file = tempDB.files.find(f => f.id === selectedFile && f.ownerId === currentUser.id);
+if (!file) return callback("Skedari nuk ekziston ose nuk është i juaji");
