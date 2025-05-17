2fc501aee3104d9b43b1ccda40a7fd0532ff9ec8
DiellartMulolli <diellart.mulolli@student.uni-pr.edu>
2025-05-17 19:50:58 +0200

    AES key generator

---
+++ script/fileManager.js
@@ -0,0 +1,9 @@
+import { currentUser } from './auth.js';
+
+export async function generateAesKey() {
+    return await window.crypto.subtle.generateKey(
+        { name: "AES-GCM", length: 256 },
+        true,
+        ["encrypt", "decrypt"]
+    );
+}
