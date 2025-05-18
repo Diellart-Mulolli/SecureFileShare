2233486a369d5fb911da08043c6f5cd1cb95b226
drenxhyliqi <113259185+drenxhyliqi@users.noreply.github.com>
2025-05-18 15:06:38 +0200

    The main logic

--- script/fileEditor.js
+++ script/fileEditor.js
@@ -1,1 +1,15 @@
 import { currentUser } from './script/auth.js';
+
+export async function decryptFileData(fileRecord, encryptedKey, tempDB) {
+    const privateKey = await window.crypto.subtle.importKey("jwk", currentUser.privateKey, {
+        name: "RSA-OAEP", hash: "SHA-256"
+    }, true, ["decrypt"]);
+    const decryptedAesKey = await window.crypto.subtle.decrypt(
+        { name: "RSA-OAEP" },
+        privateKey,
+        new Uint8Array(encryptedKey)
+    );
+    return await window.crypto.subtle.importKey("raw", decryptedAesKey, {
+        name: "AES-GCM", length: 256
+    }, true, ["decrypt"]);
+}
